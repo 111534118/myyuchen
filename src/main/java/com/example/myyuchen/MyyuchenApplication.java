037376f42aaa24a8ea341679a86cd65190feafd7
111534118 <111534118@stu.ukn.edu.tw>
2025-10-29 11:26:20 +0800

    d

---
+++ src/main/java/com/example/myyuchen/MyyuchenApplication.java
@@ -0,0 +1,13 @@
+package com.example.myyuchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyyuchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyyuchenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myyuchen/MyyuchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myyuchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyyuchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
